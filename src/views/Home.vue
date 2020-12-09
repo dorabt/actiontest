7592f13a9a4f32708f990ac49d55ab1c45c30193
dora <dora.github@gmail.com>
2020-12-09 22:06:39 +0900

    ベース完成

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    {{ fromJson }}
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import testJson from "../json/test.json";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      fromJson: "",
+    };
+  },
+  mounted() {
+    this.fromJson = testJson[testJson.length - 1].name;
+  },
+};
+</script>
